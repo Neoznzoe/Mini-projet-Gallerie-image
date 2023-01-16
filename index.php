8a9f84fca41c1da24ad5e80c11ee862b307777ec
Victor Besson <bessonvictor59@gmail.com>
2023-01-16 12:22:25 +0100

    16/01/23

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+
+    // VERIFIER SI L'IMAGE EST BIEN RECU
+
+    if(isset($_FILES['image']) && $_FILES['image']['error'] == 0){
+
+        $error = 1;
+
+        // TAILLE
+        if($_FILES['image']['size'] <= 3000000){
+
+            // EXTENSION
+            $infoImage = pathinfo($_FILES['image']['name']);
+            $extensionImage = $infoImage['extension'];
+            $extensionArray = array ('jpg', 'png', 'jpeg','gif');
+
+            if(in_array($extensionImage, $extensionArray)){
+
+                $imageName = 'uploads/'.time().rand().'.'.$extensionImage;
+
+                // ENVOYER L'IMAGE
+                move_uploaded_file($_FILES['image']['tmp_name'], $imageName);
+                $error = 0;
+
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,41 +36,32 @@
 </head>
 
 <header>
-    <p>Captureur d'image</p>
+    <h1 class="header">Captureur d'image</h1>
 </header>
 
 <body>
+    <div>
+        <h1 class="body_header">Bienvenue sur le captureur d'image</h1>
 
-    <h1>Bienvenue sur le captureur d'image</h1>
+        <form action="index.php" method="post" enctype="multipart/form-data">
+            <p>
+                <input type="file" name="image" required><br>
+                <button type="submit">Envoyer</button>
+            </p>
+        </form>
 
-    <form class="form" action="index.php" method="post" enctype="multipart/form-data">
         <?php
-        echo '
-            <tr>
-                <td>
-                    <input type="text" placeholder="Entrez votre nom" name="nom">
-                </td>
-            </tr><br/>
+            if(isset($error) && $error==0){
 
-            <tr>
-                <td>
-                    <input type="file" name="image">
-                </td>
-                </tr><br/> 
-            <button>Envoyer</button><br/>';       
-        
-            if(isset($_POST['nom']) && ($_FILES['image'])) {
-                
-                $name = htmlspecialchars($_POST['nom']);
+                echo '<div id="galery">
+                        <img src="'.$imageName.'" id="image"/>';
 
-                echo '<p class="hello"> Bonjour '.$name.' vous avez 2 nouveaux messages </p><br/>';
-                echo 'Bien envoyer le fichier : '.$_FILES['image']['name'].'';
+            } elseif(isset($error) && $error !=0){
+                echo 'Votre image ne peut pas être envoyée. Vérifiez son extension et sa taille (maximum à 3mo). </div>';
             }
-    echo '</form>
-    <p class="gallery">
-        <h1>Hello '.$name.' et bienvenue.
-    </p>';
-    ?>
+        ?>
+        
+        </div>
 
-</body>
+    </body>
 </html>
